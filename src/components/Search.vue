900f59f10c69155c5de754ffd6b09b945e8bf9e9
曾胖 <2516629927@qq.com>
2023-02-13 23:01:26 +0800

    更新Vuex示例

---
+++ src/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+    <input type="text" :value="$store.state.search" @change="onChange">
+</template>
+<script>
+  export default{
+    methods:{
+        onChange(e){
+            //提交mutation，以此修改状态
+            this.$store.commit('setSearch',e.target.value);
+            
+            this.$store.dispatch('getWeather',e.target.value);
+        }
+    }
+  }
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+const { createStore } = require("vuex");
+export default createStore({
+    //定义state，它是单一状态树————用一个对象就包含了全部的应用层级状态，主要作用是保存状态
+    state:{
+        search:'',
+        weather:{}
+    },
+    //定义getters，作用是用于访问state(状态)
+    getters:{
+        searchText(state){
+            return state.search+'市'
+        }
+    },
+    //定义mutations，作用是用于更改state(状态)
+    mutations:{
+        setWeather(state,weather)
+        {
+            state.weather=weather;
+        },
+        setSearch(state,search)
+        {
+            state.search=search;
+        }
+    },
+    //定义actions，actions中可以通过提交mutations，来修改state(状态),并且由于actions可以包含异步操作，所以一般异步请求都在actions中发起
+    actions:{
+       async getWeather({commit,state},city)
+       {
+          //发起异步请求
+          //let res=await fetch('链接'+state.search);
+          //let ret=await res.json();
+          let testValue=(Math.random()*10).toFixed(1);
+          
+          commit('setWeather',testValue)
+       }
+    }
+})
