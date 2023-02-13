944971a037845202e1bc110187070f4f792c09ac
曾胖 <2516629927@qq.com>
2023-02-14 00:21:05 +0800

    更新注释

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -10,12 +10,17 @@
 <script>
    import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
    export default{
+      //计算属性
       computed:{
+         //mapState是Vuex的辅助函数，可以把store中的数据映射到组件的计算属性中
          ...mapState(['search','weather']),
+         //mapGetters是Vuex的辅助函数，可以把getters内的方法映射到组件的计算属性中
          ...mapGetters(['searchText'])
       },
       methods:{
+        //mapMutations是Vuex的mutation的辅助函数,用于在组件中映射mutation内的方法，以便在组件内直接使用mutation里的方法
         ...mapMutations(['setSearch']),
+        //mapActions是Vuex的actions的辅助函数,用于在组件中映射actions内的方法，以便在组件内直接使用actions里的方法
         ...mapActions(['getWeather']),
         //设置默认值
         setDefault(){
